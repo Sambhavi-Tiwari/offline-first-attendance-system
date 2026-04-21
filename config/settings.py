d0f023351d7d3e4155981769b3c3042be9b0674d
Sambhavi <Sambhavi1407@gmail.com>
2026-04-21 10:53:47 +0530

    fixed authentication

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,8 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
