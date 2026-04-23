5ad1406335bec5627ed2a1264bee66f30632d229
Sambhavi <Sambhavi1407@gmail.com>
2026-04-23 17:08:45 +0530

    auth fixed

--- attendance/static/attendance/app.js
+++ attendance/static/attendance/app.js
@@ -83,7 +83,7 @@ async function login() {
     const res = await fetch("/api/login/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password })
+      body: JSON.stringify({ username:username, password:password })
     });
 
     const data = await res.json();
--- attendance/views.py
+++ attendance/views.py
@@ -145,12 +145,13 @@ def create_admin(request):
         return Response({"message": "Admin created"})
     return Response({"message": "Admin already exists"})
 
-from django.contrib.auth import authenticate
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
 @api_view(['POST'])
+@authentication_classes([])   # 🔥 IMPORTANT (removes session auth)
 @permission_classes([AllowAny])
 def login_user(request):
     username = request.data.get("username")
@@ -163,5 +164,5 @@ def login_user(request):
             "message": "Login successful",
             "user_id": user.id
         })
-    
+
     return Response({"error": "Invalid credentials"}, status=400)
